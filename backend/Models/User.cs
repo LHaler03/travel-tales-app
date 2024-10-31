0e6e5bd116d2ef61a269ab847bc800d5fa36f5a8
LHaler03 <lovro.haler@fer.unizg.hr>
2024-10-31 16:58:08 +0100

    Add necessary folder structure, models and Entity Framework

---
+++ backend/Models/User.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+    }
+}
