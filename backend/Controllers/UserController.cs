1502f0fc4bc524276225f2446e426b092a80b5ff
mislav333 <mislav.hlupic@fer.unizg.hr>
2024-10-31 18:48:09 +0100

    Testing git commit

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    public class UserController
+    {
+        public void test()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
