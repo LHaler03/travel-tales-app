b7dff014a7c869d449f4a1e0044f4ae66f6d8a1f
KristijanSagovac <kristijan.sagovac@fer.unizg.hr>
2024-11-01 14:27:26 +0100

    Added basic navbar elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { Navbar } from './components/Navbar/Navbar';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
-  return <h1>travel tales</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <Navbar />
+      <br />
+      <h1>travel tales</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar/Navbar.styled.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const NavbarStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #97BCD8;
+  padding: 20px;
+  margin: 0 8px 8px 8px;
+
+  div {
+    width: 40%;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  button {
+    background-color: #7ea1de;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-family: inherit;
+    padding: 5px;
+  }
+`;
