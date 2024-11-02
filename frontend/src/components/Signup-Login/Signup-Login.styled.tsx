6708994590c5fd6bfa5cd7f4e90861bcfa9a9b05
KristijanSagovac <kristijan.sagovac@fer.unizg.hr>
2024-11-02 18:59:15 +0100

    Modified styles for login/register
    Modified styles for login/register page to look good on mobile devices.

--- frontend/src/components/Signup-Login/Signup-Login.styled.tsx
+++ frontend/src/components/Signup-Login/Signup-Login.styled.tsx
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-  margin-top: 150px;
-  background-color: #7ea1de;
-  padding-bottom: 30px;
-  width: 600px;
-  height: 600px;
-
-  @media (max-width: 375px) {
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 3rem);
     width: 100%;
-    height: 100vh;
-    margin-top: 0;
-  }
+    margin-top: 3rem;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    margin: auto;
+    background-color: #7ea1de;
+    max-width: 90%;
+    border-radius: 20px;
 `;
 
 
@@ -22,88 +24,72 @@ export const Header = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 9px;
+    gap: 8px;
     width: 100%;
     margin-top: 30px; 
 }
 `;
 
 export const Text = styled.div`
-    font-size: 48px;
+    font-size: 2rem;
     font-weight: 700;
     color: black;
-
-    @media (max-width: 375px) {
-        font-size: 32px;
-    }
 `;
 
 
 export const Underline = styled.div`
-    width: 61px;
-    height: 6px;
+    width: 60px;
+    height: 0.5rem;
     background-color: black;
-    border-radius: 9px;
+    border-radius: 10px;
 `;
 
 export const Inputs = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 55px;
-    gap: 25px;
+    margin-top: 40px;
+    gap: 20px;
 `;
 
 export const Input = styled.div`
     display: flex;
     align-items: center;
     margin: auto;
-    width: 480px;
-    height: 80px;
+    width: 90%;
+    height: 10%;
     background-color: #b5cfe3;
     border-radius: 6px;
-
-    @media (max-width: 375px) {
-        width: 90%;
-        height: 60px;
-    }
+    padding: 5px;
 
     img {
-        margin: 0px 30px;
-        @media (max-width: 375px) {
-            margin: 0px 15px;
-        }
+        margin: 0px 20px;
+        width: 10%;
     }
+
     input {
-        height: 50px;
-        width: 400px;
         background: transparent;
         border: none;
         outline: none;
-        font-size: 20px;
+        font-size: 1rem;
         color: black;
-
-        @media (max-width: 375px) {
-            width: 100%;
-            height: 40px;
-            font-size: 16px;
-        }
+        width: 95%;
     }
 `;
 
 export const RedirectContainer = styled.div`
     display: flex;
     justify-content: center;
-    margin: 20px;
+    margin: 20px 10px 0px 10px;
     padding: 5px;
     gap: 5px;
 `
 
 export const Question = styled.div`
-    font-size: 1.2rem;
+    font-size: 1.1rem;
 `
 
 export const RedirectLink = styled.a`
-    font-size: 1.2rem;
+    font-size: 1.1rem;
 `
 
 
@@ -111,11 +97,6 @@ export const SubmitContainer = styled.div`
     display: flex;
     gap: 30px;
     margin: 30px auto;
-
-    @media (max-width: 375px) {
-        gap: 15px;
-        margin: 30px auto;
-    }
 `;
 
 export const Submit = styled.div`
@@ -130,11 +111,5 @@ export const Submit = styled.div`
    font-size: 20px;
    font-weight: 700;
    cursor: pointer;
-
-   @media (max-width: 375px) {
-        width: 150px;
-        height: 45px;
-        font-size: 16px;
-   }
 `;
 
--- frontend/src/components/Signup-Login/Signup-Login.tsx
+++ frontend/src/components/Signup-Login/Signup-Login.tsx
@@ -1,48 +1,50 @@
-import { Container, Header, Input, Inputs, Question, RedirectContainer, RedirectLink, Submit, SubmitContainer, Text, Underline } from "./Signup-Login.styled";
+import { Container, Header, Input, Inputs, Question, RedirectContainer, RedirectLink, Submit, SubmitContainer, Text, Underline, Wrapper } from "./Signup-Login.styled";
 
 export const Signup = ({action}: { action: string }) => {
   return (
     <>
-      <Container>
-          <Header>
-              <Text>{action === "login" ? "Login" : "Register"}</Text>
-              <Underline></Underline>
-          </Header>
-          <Inputs>
-            <Input>
-              <img src="./images/username.png" alt="username" />
-              <input type="text" placeholder="Marko Livaja" />
-            </Input>
-            {action === "login" ? <div></div> : (
+      <Wrapper>
+        <Container>
+            <Header>
+                <Text>{action === "login" ? "Login" : "Register"}</Text>
+                <Underline></Underline>
+            </Header>
+            <Inputs>
               <Input>
-                <img src="./images/email.png" alt="email" />
-                <input type="email" placeholder="marko.livaja@Hajduk.com" />
+                <img src="./images/username.png" alt="username" />
+                <input type="text" placeholder="John Doe" />
               </Input>
-            )}
-            <Input>
-              <img src="./images/password.png" alt="password" />
-              <input type="password" placeholder="livaja>petkovic" />
-            </Input>
-          </Inputs>
-          {action === "login" ? (
-            <RedirectContainer>
-              <Question>Don't have an account?</Question>
-              <RedirectLink href="/register">Register</RedirectLink>
-            </RedirectContainer>
-          ) : (
-            <RedirectContainer>
-              <Question>Already have an account?</Question>
-              <RedirectLink href="/login">Log in</RedirectLink>
-            </RedirectContainer>
-          )}
-          <SubmitContainer>
+              {action === "login" ? <div></div> : (
+                <Input>
+                  <img src="./images/email.png" alt="email" />
+                  <input type="email" placeholder="john.doe@gmail.com" />
+                </Input>
+              )}
+              <Input>
+                <img src="./images/password.png" alt="password" />
+                <input type="password" placeholder="testpw123" />
+              </Input>
+            </Inputs>
             {action === "login" ? (
-              <Submit>Login</Submit>
+              <RedirectContainer>
+                <Question>Don't have an account?</Question>
+                <RedirectLink href="/register">Register</RedirectLink>
+              </RedirectContainer>
             ) : (
-              <Submit>Register</Submit>
+              <RedirectContainer>
+                <Question>Already have an account?</Question>
+                <RedirectLink href="/login">Log in</RedirectLink>
+              </RedirectContainer>
             )}
-          </SubmitContainer>
-      </Container>
+            <SubmitContainer>
+              {action === "login" ? (
+                <Submit>Login</Submit>
+              ) : (
+                <Submit>Register</Submit>
+              )}
+            </SubmitContainer>
+        </Container>
+      </Wrapper>
     </>
   );
 }
