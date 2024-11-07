e080f77f9368bb34168ab40698a9bb2cea06b669
dujebudiselic <duje.budiselic@fer.unizg.hr>
2024-11-07 22:29:28 +0100

    axios get location
    Dohvat lokacija

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,36 +1,39 @@
-import 'leaflet/dist/leaflet.css'
-import { TileLayer, Marker } from "react-leaflet"
+import 'leaflet/dist/leaflet.css';
+import { TileLayer, Marker} from 'react-leaflet';
 import { StyledMapContainer } from './Map.styled';
-/*import MarkerClusterGroup from "react-leaflet-cluster"; */
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const Map = () => {
-    const markers: { geocode: [number, number]; popUp: string }[] = [
-        {
-          geocode: [48.8667, 2.3167],
-          popUp: "Paris"
-        },
-        {
-          geocode: [41.3850639, 2.1734035],
-          popUp: "Barcelona"
-        },
-        {
-          geocode: [51.5073509, -0.1277583],
-          popUp: "London"
-        }
-      ];
+    const [markers, setMarkers] = useState<{ coordinates: [number, number]; popUp: string }[]>([]);
+
+    useEffect(() => {
+        const fetchLocations = async () => {
+            try {
+                const response = await axios.get('http://localhost:5185/api/locations');
+                const locations = response.data.map((location: { latitude: number; longitude: number; name: string }) => ({
+                    coordinates: [location.latitude, location.longitude] as [number, number],
+                    popUp: location.name,
+                }));
+
+                setMarkers(locations);
+            } catch (error) {
+                console.error("Error fetching locations:", error);
+            }
+        };
+
+        fetchLocations(); 
+    }, []);
+
     return (
         <StyledMapContainer id="map" center={[54.5260, 15.2551]} zoom={3}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {/*<MarkerClusterGroup>*/}
-                {markers.map((marker, index) => (
-                    <Marker key={index} position={marker.geocode} />
-                ))}
-            {/*</MarkerClusterGroup>*/}
-            
+            {markers.map((marker, index) => (
+                <Marker key={index} position={marker.coordinates}/>
+            ))}
         </StyledMapContainer>
-
-    )
-}
+    );
+};
