d8ac3ad677bba7c51ca42a18a3712ec7f7a82a44
LHaler03 <lovro.haler@fer.unizg.hr>
2024-11-12 11:29:57 +0100

    used index as key in the map function of the `Card` for rendering list items and added a new migration to support refresh tokens for google-signin

---
+++ backend/Migrations/20241112101358_UpdatingAspNetUsers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7502f723-5a5a-48a4-bb3e-260139296c2a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f9f641c7-ac92-4b1d-9b6a-3d90a8f84d84");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiry",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "81c91aae-81f9-4363-8886-7394c0c6f6de", null, "Admin", "ADMIN" },
+                    { "cb697d1e-c66c-4894-aa28-4303d3d9bfbb", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "81c91aae-81f9-4363-8886-7394c0c6f6de");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cb697d1e-c66c-4894-aa28-4303d3d9bfbb");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiry",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7502f723-5a5a-48a4-bb3e-260139296c2a", null, "Admin", "ADMIN" },
+                    { "f9f641c7-ac92-4b1d-9b6a-3d90a8f84d84", null, "User", "USER" }
+                });
+        }
+    }
+}
