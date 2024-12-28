c00d592c01f6f929b566ec87099773adb7b05192
fs55266 <florijan.stankir@fer.unizg.hr>
2024-12-28 20:06:50 +0100

    update2

--- frontend/src/components/AdminWeb/AdminUsers/UsersReview.styled.tsx
+++ frontend/src/components/AdminWeb/AdminUsers/UsersReview.styled.tsx
@@ -1,25 +1,134 @@
 import styled from 'styled-components';
 
+
 export const UserList = styled.ul`
     list-style-type: none;
-    padding: 0;
+    padding: 2rem;
+`;
+
+export const Title = styled.h1`
+  padding: 1.3rem;
+  padding-bottom: 0;
+  font-size: 1.6rem;
+  font-weight: 600;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export const UserItem = styled.li`
     display: flex;
     justify-content: space-between;
     padding: 10px;
-    border-bottom: 1px solid #ccc;
+    border-bottom: 1px solid black;
+    margin-bottom: 10px;
+
+    @media (max-width: 600px) {
+      flex-direction: column;
+      align-items: flex-start;
+    }
 `;
 
-export const ReviewButton = styled.button`
-    background-color: #007bff;
-    color: white;
-    border: none;
-    padding: 5px 10px;
-    cursor: pointer;
+export const VerificationText = styled.div`
+    color: red;
+`;
 
-    &:hover {
-        background-color: #0056b3;
-    }
+
+export const RedActionButton = styled.button`
+  background-color: #007bff;
+  border: none;
+  border-radius: 30px;
+  padding: 8px 16px;
+  font-family: inherit;
+  font-size: 14px;
+  cursor: pointer;
+  color: white;
+  transition: all 0.3s ease;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    background-color: #66aaff;
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+
+  &:active {
+    background-color: #0056b3;
+    transform: scale(0.95);
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  }
+
+  @media (min-width: 600px) {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+`;
+
+export const EmailLink = styled.a`
+  color: black;
+  text-decoration: none;
+  margin-left: 0.5rem;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+  padding-left: 2rem;
+  padding-right: 3rem;
+`;
+
+export const SearchInput = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 1rem;
+  flex-grow: 1;
+  max-width: 30rem;
+  transition: border-color 0.3s ease;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+`;
+
+export const SearchButton = styled(RedActionButton)`
+  padding: 10px 20px;
+  background-color: black;
+
+  &:hover {
+    background-color: #555;
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  }
+
+  &:active {
+    background-color: #333;
+    transform: scale(0.95);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+  }
+
+  @media (min-width: 600px) {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+`;
+
+export const ProfileReviewButton = styled(RedActionButton)`
+  margin-top: 1rem;
+  padding: 10px;
+  padding-top: 10rem;
+
+  @media (min-width: 600px) {
+    display: none;
+  }
 `;
--- frontend/src/components/AdminWeb/AdminUsers/UsersReview.tsx
+++ frontend/src/components/AdminWeb/AdminUsers/UsersReview.tsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { UserList, UserItem, ReviewButton } from './UsersReview.styled';
+import { useState, useEffect } from 'react';
+import { UserList, UserItem, Title, RedActionButton, VerificationText, EmailLink, SearchContainer, SearchInput, SearchButton } from './UsersReview.styled';
 import { UserType } from '../../../context/AuthContext';
 import axios from 'axios';
 
 const UsersReview = () => {
   const [users, setUsers] = useState<UserType[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://3.74.155.131/api/account/users');
+        const response = await axios.get('http://3.74.155.131/api/users');
         setUsers(response.data);
       } catch (error) {
         console.error('Error fetching users:', error);
@@ -19,15 +20,44 @@ const UsersReview = () => {
     fetchUsers();
   }, []);
 
+  const filteredUsers = users.filter(user =>
+    user.email?.toLowerCase().includes(searchTerm.toLowerCase()) ?? false
+  );
+
   return (
-    <UserList>
-      {users.map((user: UserType) => (
-        <UserItem key={user.username}>
-          {user.username}
-          <ReviewButton onClick={() => handleReview(user.username)}>Review Profile</ReviewButton>
-        </UserItem>
-      ))}
-    </UserList>
+    <>
+      <Title>
+        <h1>Travel Tales users</h1>
+      </Title>
+      <SearchContainer>
+        <SearchInput
+          type="text"
+          placeholder="Search Users..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <SearchButton onClick={() => console.log('Searching...')}>
+          Search
+        </SearchButton>
+      </SearchContainer>
+      <UserList>
+        {filteredUsers.map((user: UserType) => (
+          <UserItem key={user.username}>
+            <EmailLink href={user.email ?? '#'}>
+              {user.email ?? 'No email'}
+            </EmailLink>
+            {/* {user.emailConfirmed ? (
+              <VerificationText>Email verified</VerificationText>
+            ) : (
+              <VerificationText>Email not verified</VerificationText>
+            )} */}
+            <RedActionButton onClick={() => handleReview(user.username)}>
+              Review Profile
+            </RedActionButton>
+          </UserItem>
+        ))}
+      </UserList>
+    </>
   );
 };
 
