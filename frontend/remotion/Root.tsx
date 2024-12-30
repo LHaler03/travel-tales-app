815c8552028dc9ea686b5dae897785bafaee64b8
KristijanSagovac <kristijan.sagovac@fer.unizg.hr>
2024-12-30 17:56:23 +0100

    Added vertical postcard prototype

--- frontend/remotion/Root.tsx
+++ frontend/remotion/Root.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Still } from 'remotion';
 import { ImageSchema, MyImage } from './Image';
+import { VerticalImage } from './VerticalImage';
  
 export const RemotionRoot: React.FC = () => {
   return (
-    <Still
-      schema={ImageSchema}
-      defaultProps={{
-        fromText: 'travel tales',
-        titleColor: 'black',
-        fromColor: 'black',
-        borderColor: 'white',
-      }}
-      id="MyImage"
-      component={MyImage}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Still
+        schema={ImageSchema}
+        defaultProps={{
+          fromText: 'travel tales',
+          titleColor: 'black',
+          fromColor: 'black',
+          borderColor: 'white',
+        }}
+        id="MyImage"
+        component={MyImage}
+        width={1920}
+        height={1080}
+      />
+
+      <Still
+        schema={ImageSchema}
+        defaultProps={{
+          fromText: 'travel tales',
+          titleColor: 'black',
+          fromColor: 'black',
+          borderColor: 'white',
+        }}
+        id="VerticalImage"
+        component={VerticalImage}
+        width={1080}
+        height={1920}
+      />
+    </>
   );
 };
---
+++ frontend/remotion/VerticalImage.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Img, staticFile } from 'remotion'
+import { loadFont } from "@remotion/google-fonts/Nunito";
+import { z } from "zod";
+import { ImageSchema } from './Image';
+
+const { fontFamily } = loadFont();
+
+export const VerticalImage: React.FC<z.infer<typeof ImageSchema>> = ({
+  fromText: text1,
+  titleColor: color1,
+  fromColor: color2,
+  borderColor: color3,
+}) => {
+    return (
+       <div 
+          style={{ 
+            position: 'relative', 
+            width: '1080px', 
+            height: '1920px', 
+            overflow: 'hidden', 
+            border: `12px solid ${color3}` 
+          }}>
+         <Img 
+           src={staticFile("images/1.avif")} 
+           style={{ 
+            position: 'absolute', 
+            top: 0, 
+            left: 0, 
+            height: '60%', 
+            width: 'auto', 
+            objectFit: 'cover' 
+          }} 
+         />
+         <div 
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              right: 0,
+              width: '100%',
+              height: '56%',
+              clipPath: 'polygon(0% 16%, 100% 0%, 100% 100%, 0% 100%)',
+              backgroundColor: color3
+            }} />
+         <Img 
+           src={staticFile("images/2.jpg")} 
+           style={{ 
+            position: 'absolute', 
+            bottom: 0, 
+            right: 0, 
+            height: '55%', 
+            width: 'auto', 
+            objectFit: 'cover', 
+            clipPath: 'polygon(0% 25%, 100% 0%, 100% 100%, 0% 100%)'
+          }} 
+         />
+         <div id="upperbox" 
+            style={{
+              position: 'absolute', 
+              top: 0, 
+              left: 0, 
+              padding: '35px', 
+              background: 'rgba(255, 255, 255, 0.3)'
+            }}>
+          <div 
+            style={{
+              fontSize: '70px', 
+              margin: '20px', 
+              fontWeight: 'bold', 
+              fontFamily: 'Nunito, sans-serif',
+              color: color1
+            }}>Berlin</div>
+          <div 
+            style={{
+              fontSize: '50px', 
+              margin: '20px', 
+              fontFamily: 'Nunito, sans-serif',
+              color: color2
+            }}>from: {text1} </div>
+         </div>
+       </div>
+    );
+   }
