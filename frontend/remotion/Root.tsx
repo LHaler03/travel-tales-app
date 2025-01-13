1c209092db7f062dc64fc8401ccd73f7f0407822
KristijanSagovac <kristijan.sagovac@fer.unizg.hr>
2025-01-13 15:21:07 +0100

    Added postcards with only 1 image

--- frontend/remotion/Root.tsx
+++ frontend/remotion/Root.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Still } from 'remotion';
 import { VerticalImage } from './VerticalImage';
 import { HorizontalImage, ImageSchema } from './HorizontalImage';
+import { Horizontal1Image } from './Horizontal1Image';
+import { Vertical1Image } from './Vertical1Image';
  
 export const RemotionRoot: React.FC = () => {
   return (
@@ -32,6 +34,36 @@ export const RemotionRoot: React.FC = () => {
         }}
         id="VerticalImage"
         component={VerticalImage}
+        width={1080}
+        height={1920}
+      />
+
+      <Still
+        schema={ImageSchema}
+        defaultProps={{
+          fromText: 'travel tales',
+          titleColor: 'black',
+          fromColor: 'black',
+          borderColor: 'white',
+          cityName: 'Berlin'
+        }}
+        id="Horizontal1Image"
+        component={Horizontal1Image}
+        width={1920}
+        height={1080}
+      />
+
+      <Still
+        schema={ImageSchema}
+        defaultProps={{
+          fromText: 'travel tales',
+          titleColor: 'black',
+          fromColor: 'black',
+          borderColor: 'white',
+          cityName: 'Berlin'
+        }}
+        id="Vertical1Image"
+        component={Vertical1Image}
         width={1080}
         height={1920}
       />
